5c8b1f0a49cd300eda9099dd9915d6037521da4f
박현찬 <hyunchan@baghyeonchan-ui-MacBookAir.local>
2021-06-22 00:22:24 +0900

    Add : 초기 설정

--- src/Styles/GlobalStyle.js
+++ src/Styles/GlobalStyle.js
@@ -7,11 +7,6 @@ const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
   }
-  
-  body{
-    background-color: #ffffff;
-    font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  }
 
   a {
     color: inherit;
@@ -24,18 +19,12 @@ const GlobalStyle = createGlobalStyle`
     outline: none;
   }
 
-  h1, h2, h3, h4, h5, h6{
-    font-family:'Maven Pro', sans-serif;
-  }
-
   ol, ul, li {
     list-style: none;
   }
 
   img {
-    display: block;
     width: 100%;
-    height: 100%;
   }
 `;
 
