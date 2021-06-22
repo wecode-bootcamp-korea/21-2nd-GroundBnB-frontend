c9052e2b131f26d1e50eb66fb424e6170250fb3e
박현찬 <hyunchan@baghyeonchan-ui-MacBookAir.local>
2021-06-22 11:02:57 +0900

    Add : 초기재설정

--- src/Routes.js
+++ src/Routes.js
@@ -1,25 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
 import Detail from './Pages/Detail/Detail';
 import Result from './Pages/Result/Result';
 import Main from './Pages/Main/Main';
-import GlobalStyle from './Styles/GlobalStyle';
-import theme from './Styles/theme';
 
 function Routes() {
   return (
-    <GlobalStyle>
-      <ThemeProvider theme={theme}>
-        <Router>
-          <Switch>
-            <Route exact path="/main" component={Main} />
-            <Route exact path="/result" component={Result} />
-            <Route exact path="/detail" component={Detail} />
-          </Switch>
-        </Router>
-      </ThemeProvider>
-    </GlobalStyle>
+    <Router>
+      <Switch>
+        <Route exact path="/main" component={Main} />
+        <Route exact path="/result" component={Result} />
+        <Route exact path="/detail" component={Detail} />
+      </Switch>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
 import Routes from './Routes';
+import GlobalStyle from './Styles/GlobalStyle';
+import theme from './Styles/theme';
 
-ReactDOM.render(<Routes />, document.getElementById('root'));
+ReactDOM.render(
+  <>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <Routes />
+    </ThemeProvider>
+  </>,
+  document.getElementById('root'),
+);
