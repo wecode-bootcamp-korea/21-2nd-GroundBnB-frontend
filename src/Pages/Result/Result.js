6cc0773f81efa692840bcaa641a0a155ac380bcb
hyunchanpark <eye3570@gmail.com>
2021-06-30 10:50:35 +0900

    Modify :검색바인풋창 데이터 실시간 db연동 test 성공
    Modify : 주석 제거 및 스타일링 수정
    
    Modify : API naming 직관적인 네이밍으로 수정
    
    Modify : API 세부적으로 분할
    
    Modify : API 수정

--- src/Pages/Result/Result.js
+++ src/Pages/Result/Result.js
@@ -1,35 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import { GET_SUBMIT_SEARCHED_ROOMS_API } from '../../config';
 import Map from '../../Components/Map/Map';
 
 function Result() {
   const location = useLocation();
   const [roomData, setRoomData] = useState([]);
 
-  // useEffect(() => {
-  //   fetch(`http://10.58.6.210:8000/rooms${location.search}`, {
-  //     method: 'GET',
-  //     headers: { 'Content-Type': 'application/json' },
-  //   })
-  //     .then((res) => res.json())
-  //     .then((result) => {
-  //       setRoomData(result.rooms.rooms);
-  //     });
-  // }, []);
-
   useEffect(() => {
-    fetch('./data/roomData.json')
+    fetch(`${GET_SUBMIT_SEARCHED_ROOMS_API}${location.search}`, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+    })
       .then((res) => res.json())
       .then((result) => {
         setRoomData(result.rooms.rooms);
       });
-  }, []);
+  }, [location.search]);
 
-  return (
-    <div>
-      <Map rooms={roomData} />
-    </div>
-  );
+  // Mock Data fetch
+  // useEffect(() => {
+  //   fetch('./data/roomData.json')
+  //     .then((res) => res.json())
+  //     .then((result) => {
+  //       console.log('result page', result.rooms.rooms);
+  //       setRoomData(result.rooms.rooms);
+  //     });
+  // }, []);
+
+  return <div>{roomData.length && <Map rooms={roomData} />}</div>;
 }
 
 export default Result;
