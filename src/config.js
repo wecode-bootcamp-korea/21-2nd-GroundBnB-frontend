a37f0ed2a907f66d5339ed731013414fabb791b2
hyunchanpark <eye3570@gmail.com>
2021-06-30 10:50:35 +0900

    Modify :검색바인풋창 데이터 실시간 db연동 test 성공
    Modify : 주석 제거 및 스타일링 수정
    
    Modify : API naming 직관적인 네이밍으로 수정
    
    Modify : API 세부적으로 분할
    
    Modify : API 수정

--- src/config.js
+++ src/config.js
@@ -2,8 +2,6 @@ const BASE_URL = 'http://10.58.3.69:8000';
 
 export const KAKAO_LOGIN_API = `${BASE_URL}/users/login/kakao`;
 
-export const GET_SEARCHED_ROOMS_API = `${BASE_URL}/rooms`;
+export const GET_SUBMIT_SEARCHED_ROOMS_API = `${BASE_URL}/rooms`;
 
-const config = { KAKAO_LOGIN_API, GET_SEARCHED_ROOMS_API };
-
-export default config;
+export const GET_INPUTTED_ROOMS_API = `${BASE_URL}/rooms/searchword`;
